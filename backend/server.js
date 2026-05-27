e9035f228096765b74fc853fedeb975d2b631ace
Виктория <vikastrizkova@gmail.com>
2026-05-27 17:40:48 +0300

    подключинение

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,12 @@ const { Server } = require('socket.io');
 const cors = require('cors');
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: ' }));
 
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST']
   }
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3001');
+const socket = io('https://hand-status-realtime.onrender.com');
 
 function App() {
   const [inHand, setInHand] = useState(null);
